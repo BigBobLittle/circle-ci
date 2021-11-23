482a772aff953f4910b63666ca07e6534066e828
BigBobLittle <littletheprogrammer@gmail.com>
2021-11-23 07:15:57 +0000

    docs: updating docs to meet conventional message styles

--- tests/space.spec.js
+++ tests/space.spec.js
@@ -1,7 +1,7 @@
-const request = require('supertest');
-const app = require('../app');
+const request = require("supertest");
+const app = require("../app");
 
-describe('Space test suite', () => {
+describe("Space test suite", () => {
   // extend jest matchers
   expect.extend({
     toBeActive: (received) => {
@@ -19,44 +19,44 @@ describe('Space test suite', () => {
       };
     },
   });
-  test('test /destination endpoint', async () => {
-    const response = await request(app).get('/space/destinations');
+  test("test /destination endpoint", async () => {
+    const response = await request(app).get("/space/destinations");
 
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual([
-      'Mars',
-      'Moon',
-      'Earth',
-      'Mercury',
-      'Venus',
-      'Jupiter',
+      "Mars",
+      "Moon",
+      "Earth",
+      "Mercury",
+      "Venus",
+      "Jupiter",
     ]);
     expect(response.body).toHaveLength(6);
-    expect(response.body).toEqual(expect.arrayContaining(['Earth']));
+    expect(response.body).toEqual(expect.arrayContaining(["Earth"]));
   });
 
-  test('test /space dot flights', async () => {
-    const response = await request(app).get('/space/flights/seats');
+  test("test /space dot flights", async () => {
+    const response = await request(app).get("/space/flights/seats");
 
     expect(response.statusCode).toBe(200);
 
     // expect an array of various objects
     expect(response.body.starship).toEqual(
-      expect.arrayContaining([expect.any(Object)]),
+      expect.arrayContaining([expect.any(Object)])
     );
 
     // array will contain firstClass object
     expect(response.body.starship).toEqual(
       expect.arrayContaining([
         expect.objectContaining({ firstClass: expect.any(Object) }),
-      ]),
+      ])
     );
 
     // array will contain business class object
     expect(response.body.starship).toEqual(
       expect.arrayContaining([
         expect.objectContaining({ businessClass: expect.any(Object) }),
-      ]),
+      ])
     );
 
     // under businessClass object check if drinkServed object is present and contains array
@@ -67,20 +67,21 @@ describe('Space test suite', () => {
             drinksServed: expect.any(Array),
           }),
         }),
-      ]),
+      ])
     );
 
-    // Checking that under the firstClass: Object we have the option ludacris in the seatHover Object
+    // Checking that under the firstClass: Object we have the option ludacris in
+    // the seatHover Object
     expect(response.body.starship).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
           firstClass: expect.objectContaining({
             seatHover: expect.objectContaining({
-              cryoMode: expect.arrayContaining(['ludacris']),
+              cryoMode: expect.arrayContaining(["ludacris"]),
             }),
           }),
         }),
-      ]),
+      ])
     );
 
     // Checking that under the firstClass: Object we have the option plaid in the seatHover Object
@@ -89,16 +90,16 @@ describe('Space test suite', () => {
         expect.objectContaining({
           firstClass: expect.objectContaining({
             seatHover: expect.objectContaining({
-              cryoMode: expect.arrayContaining(['plaid']),
+              cryoMode: expect.arrayContaining(["plaid"]),
             }),
           }),
         }),
-      ]),
+      ])
     );
   });
 
-  it('test toBeActive flights endpoint', async () => {
-    const response = await request(app).get('/space/flights');
+  it("test toBeActive flights endpoint", async () => {
+    const response = await request(app).get("/space/flights");
 
     expect(response.body[0].active).toBeActive();
     expect(response.body[1].active).not.toBeActive();
